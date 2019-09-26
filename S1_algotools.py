2da1d1f34158cd9530ebc8755c8d614b544ce410
Kilian Chamiot-Maitral <kikicm@hotmail.fr>
2019-09-26 15:54:10 +0200

    average_above_zero function

--- S1_algotools.py
+++ S1_algotools.py
@@ -9,3 +9,36 @@ print('Hello World')
 
 myVariable=0
 print('myVariable=', myVariable)
+
+# Averaging
+
+'''
+- What happens if Som initialization is forgotten ?
+The script will work but the result will not be correct
+
+- What can you expect if all the values are below zero ?
+Error : divide by 0 is impossible (N keep 0 as value)
+
+'''
+import numpy as np
+
+
+def average_above_zero(table:list):
+    ##
+    #Function that calculates the table average without values < 0
+    #Args:
+    #    @param table: the list to calculate the average
+    #Returns the average    
+    som=0
+    n=0
+    
+    for i in range(len(table)):
+        if table[i] > 0:
+            som += table[i]
+            n += 1    
+    return som/n
+
+tab_list=np.array([1,2,3,-4,6,-9])
+#tab_zero=np.zeros(12, dtype=np.int16)
+
+print('Average above 0 : {average} '.format(average=average_above_zero(tab_list)))
