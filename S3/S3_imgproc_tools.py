b675ffd33d9122e1c3db085d7e5e08c187e671aa
Kilian Chamiot-Maitral <kikicm@hotmail.fr>
2019-10-15 13:35:57 +0200

    Add timer to measure runtime

--- S3/S3_imgproc_tools.py
+++ S3/S3_imgproc_tools.py
@@ -7,6 +7,7 @@ Created on Mon Sep 30 13:39:33 2019
 
 import cv2
 import numpy as np
+from timeit import Timer
 
 #img_name="images/fujiyama_volcano.jpg"
 img_name="images/4k.jpg"
@@ -54,6 +55,21 @@ def invert_colors_cv2(input_img):
                 
 #inverted_colors_img = invert_colors_manual(img_bgr)
 #inverted_colors_img = invert_colors_numpy(img_bgr)
-inverted_colors_img = invert_colors_cv2(img_bgr)
-cv2.imshow("Inverted colors", inverted_colors_img)
-cv2.waitKey()                
+#inverted_colors_img = invert_colors_cv2(img_bgr)
+#cv2.imshow("Inverted colors", inverted_colors_img)
+#cv2.waitKey()   
+
+t = Timer(lambda: invert_colors_manual(img_bgr))
+print(t.timeit(number=1))      
+
+t = Timer(lambda: invert_colors_numpy(img_bgr))
+print(t.timeit(number=1)) 
+
+t = Timer(lambda: invert_colors_cv2(img_bgr))
+print(t.timeit(number=1))
+
+'''
+Results :
+0.017358299999955307
+0.017614799999932984
+0.009377900000004047'''     
