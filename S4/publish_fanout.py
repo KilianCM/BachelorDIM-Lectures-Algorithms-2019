c8df74276df3c1283da35da12c66d27f413258e7
Kilian Chamiot-Maitral <kikicm@hotmail.fr>
2019-10-21 09:18:41 +0200

    Publish fanout script

---
+++ S4/publish_fanout.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 21 08:46:41 2019
+
+@author: chamiotk
+"""
+
+import os
+import pika
+import config
+import amqp_utils as amqp
+
+def publish_fanout():
+    connection = amqp.connection()
+    channel = connection.channel()
+    channel.exchange_declare(exchange='posts', exchange_type='fanout')
+    channel.basic_publish(exchange='posts', routing_key='key', body="This is a message")
+    print("Sent message to posts exchange")
+
+    
+    
+publish_fanout()
